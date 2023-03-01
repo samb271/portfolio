e0c273e1e5bf6e4f9fb1d2b6624792ba296d4046
samb271 <footballsam1997@hotmail.com>
2023-03-01 11:40:08 -0500

    navbar positionning

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
-  <div class="navbar fixed bottom-16 left-1/2 h-14 w-fit flex rounded-full">
+  <div id="navbar" class="navbar fixed bottom-16 h-14 w-fit flex rounded-full">
     <div class="w-36 text-2xl text-center grid place-items-center">Work</div>
     <div class="w-36 text-2xl text-center grid place-items-center">Projects</div>
     <div class="w-36 text-2xl text-center grid place-items-center">Play</div>
@@ -12,5 +13,7 @@
 <style scoped>
 .navbar{
   background-color: #D9D9D9;
+  left: 50%;
+  transform: translate(-50%, 0);
 }
 </style>
