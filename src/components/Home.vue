906fa3b1ddc8c935e224b4f2ecc30bb6c5a79d62
samb271 <footballsam1997@hotmail.com>
2023-03-01 12:04:08 -0500

    home positioning fixes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <div>
+  <div class="border h-screen pb-[57px]">
     <p class="text-8xl mt-32 w-full max-w-7xl welcomeTitle">Hi! I'm <span class="sam">Sam</span>, Montreal based <span class="bg-[#5DA432] rounded-[40px] px-6">software</span> engineer.</p>
-    <p class="welcomeSubtext text-3xl mt-12">Welcome to my portofolio</p>
+    <p class="welcomeSubtext text-3xl mt-12">Welcome to my portfolio</p>
   </div>
 </template>
 
