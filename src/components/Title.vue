8c5ecfd680adfa1ba8f1765119642bb200704153
samb271 <footballsam1997@hotmail.com>
2023-03-01 12:04:14 -0500

    title component init

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p class="title text-white ">
+    <slot></slot>
+  </p>
+</template>
+
+<style scoped>
+.title{
+  font-family: "Parabole Display";
+  font-size: 96px;
+}
+</style>
