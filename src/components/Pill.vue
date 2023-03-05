441627768ca063176932783779d8b52e3b98fe38
samb271 <footballsam1997@hotmail.com>
2023-03-05 11:59:52 -0500

    pill component init

---
+++ src/components/Pill.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+const { borderColor, color } = defineProps({
+  borderColor: {
+    type: String
+  }, 
+  color: {
+    type: String
+  }
+})
+
+</script>
+
+<template>
+  <div class="text-base border-2 border-[borderColor] bg-[color] h-10 w-fit px-4 grid place-items-center rounded-full">
+    <slot></slot>
+  </div>
+</template>
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import Title from './Title.vue';
 import Section from './Section.vue';
+import Pill from './Pill.vue'
 </script>
 
 <template>
   <div class="border h-screen">
     <Title>Work</Title>
     <Section time="2020 - 2023">Powerhealth Solutions</Section>
+    <div class="border border-white">
+      <Pill border-color="#42B883" color="#143929">Vue 3</Pill>
+    </div>
   </div>
 </template>
