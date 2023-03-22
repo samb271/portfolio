68396a6b91a3e2808b3b9a5776dfa7f2064e1d19
Samuel Barbeau <samuel.barbeau@powerhealthsolutions.com>
2023-03-22 10:25:04 -0400

    feat: adds text component to use in sections

---
+++ src/components/Text.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+const { title } = defineProps({
+  title: {
+    type: String
+  },
+  year: {
+    type: String
+  }
+})
+
+</script>
+
+<template>
+  <div class="text-text-secondary text-lg max-w-3xl mt-10">
+    <div v-if="title" class="text-text-primary pb-4 flex flex-row items-center text-2xl">
+      <p>{{ title }}</p>
+      <div v-if="year" class="bg-text-primary w-2 h-2 rounded-full mt-1.5 mx-2.5"></div>
+      <p>{{ year }}</p>
+    </div>
+    <slot></slot>
+  </div>
+</template>
