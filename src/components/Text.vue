616f5f72c005d2f57df7b74a8e64e7fd1b24f6e8
Samuel Barbeau <samuel.barbeau@powerhealthsolutions.com>
2023-03-26 13:50:03 -0400

    fix: resizes text component

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -13,7 +13,7 @@ const { title } = defineProps({
 
 <template>
   <template v-if="year">
-    <div class="text-text-secondary text-lg max-w-3xl mt-10">
+    <div class="text-text-secondary text-lg max-w-2xl mt-10">
       <div v-if="title" class="text-text-primary pb-4 flex flex-row items-center text-2xl">
         <p>{{ title }}</p>
         <div class="bg-text-secondary w-1.5 h-1.5 rounded-sm mt-1 mx-2.5"></div>
