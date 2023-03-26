0d918dcd83ce21a7a919e5c7b493bde070f25c42
Samuel Barbeau <samuel.barbeau@powerhealthsolutions.com>
2023-03-26 13:49:49 -0400

    feat: adds z value to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div id="navbar" class="navbar fixed bottom-10 h-12 w-full max-w-sm flex rounded-full">
+  <div id="navbar" class="navbar z-50 fixed bottom-10 h-12 w-full max-w-sm flex rounded-full">
     <div class="w-28 mx-2 my-2 bg-navbar-contrast text-navbar-textSelected text-xl text-center rounded-full grid place-items-center">Work</div>
     <div class="w-28 mx-2 my-2 text-xl text-center grid place-items-center">Projects</div>
     <div class="w-28 mx-2 my-2 text-xl text-center grid place-items-center">Play</div>
