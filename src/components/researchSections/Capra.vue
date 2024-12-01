ded5d2b0feff8e4945d569e60d20bf2a7247057a
Samuel Barbeau <samuel.barbeau@powerhealthsolutions.com>
2024-12-01 17:11:55 -0500

    adds new capra description

--- src/components/researchSections/Capra.vue
+++ src/components/researchSections/Capra.vue
@@ -16,8 +16,13 @@ const isAgePredictionVisible = ref(true);
         <div>
           <Section time="2024 - Current"><a href="https://www.clubcapra.com">Club Capra</a></Section>
 
-          <Text title="Machine Learning Developper">
-            Spearheaded the pose prediction project, currently working on end-to-end parameterization with RL.
+          <Text title="Head of AI team">
+            Currently building Shepherd, a computer vision pipeline and world modeling system that enables our robot to understand and reason about its environment. 
+            The system integrates state-of-the-art models, including CLIP, FastSAM, YOLOWorld, BLIP, and LLama. 
+            Future capabilities will include RL-based self-navigation, with plans to compete in the 2025 European Robotics Hackathon (Enrich) in Austria.<br><br>
+            Designed and implemented a pose detection algorithm to enable visual command interpretation. Built on a fine-tuned YOLOv8 with a custom classification head, 
+            the algorithm achieved 99% accuracy and competed in the 2024 European Land Robot Trial in Germany. This marked the club’s 
+            first in-house implementation of artificial intelligence.
           </Text>
         </div>
       </div>
